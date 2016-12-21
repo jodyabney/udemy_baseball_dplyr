e334349db3c0eddda19528bd2085cad4821e3202
Jody Abney <jodyabney@iname.com>
2016-12-20 21:12:18 -0500

    After completing section 3

--- course_scripts.R
+++ course_scripts.R
@@ -32,3 +32,32 @@ bdat <- Batting %>%
   summarise(number_of_records=n()) %>%
   arrange(desc(number_of_records))
 bdat
+
+# Section 3, Lecture 14 WHERE vs HAVING
+bdat <- Batting %>%
+  filter(AB >= 400) %>%
+  group_by(playerID) %>%
+  summarise(min_career_SO=min(SO, na.rm=TRUE)) %>%
+  filter(min_career_SO < 20) %>%
+  arrange(min_career_SO)
+bdat
+
+# Section 3, Lecture 15 Batting Avg for Record
+bdat <- Batting %>%
+  filter(AB >= 400) %>%
+  mutate(batting_avg=round(H/AB, 3)) %>%
+  select(playerID, batting_avg, yearID) %>%
+  arrange(desc(batting_avg))
+head(bdat)
+
+# Section 3, Lecture 16 Career Batting Avg
+bdat <- Batting %>%
+  group_by(playerID) %>%
+  summarise(career_AB=sum(AB, na.rm=TRUE), career_H=sum(H, na.rm=TRUE)) %>%
+  filter(career_AB > 1000) %>%
+  mutate(career_batting_avg=round(career_H/career_AB, 3)) %>%
+  select(playerID, career_batting_avg) %>%
+  arrange(desc(career_batting_avg))
+head(bdat)
+
+
